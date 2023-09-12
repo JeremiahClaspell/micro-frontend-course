1ca8257241d091a85a1b56aa6b6ae0c4c4379d32
Jeremiah Claspell <jeremiah@james-enterprise.com>
2023-09-12 10:32:37 -0600

    styling fix

--- packages/marketing/src/app.tsx
+++ packages/marketing/src/app.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+	StylesProvider,
+	createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+//this is used to get @material to prefix the auto-generated class names with the name of the service
+//to prevent naming conflicts between other micro-frontends.
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'marketing',
+});
+
 export default () => {
 	return (
 		<div>
-			<StylesProvider>
+			<StylesProvider generateClassName={generateClassName}>
 				<BrowserRouter>
 					<Switch>
 						<Route exact path="/pricing" component={Pricing} />
