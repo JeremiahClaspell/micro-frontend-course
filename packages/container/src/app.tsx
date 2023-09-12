a59ffe5ae5c5958d844500c5cb4cab2f22a8a118
Jeremiah Claspell <jeremiah@james-enterprise.com>
2023-09-12 09:35:20 -0600

    added header

--- packages/container/src/app.tsx
+++ packages/container/src/app.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/marketingApp';
+import Header from './components/Header';
 
 export default () => {
 	return (
-		<div>
-			<h1>Container</h1>
-			<hr />
-			<MarketingApp />
-		</div>
+		<BrowserRouter>
+			<div>
+				<h1>Container</h1>
+				{/* props are non interactive and being used as placehoder values */}
+				<Header signedIn={false} onSignOut={() => {}} />
+				<MarketingApp />
+			</div>
+		</BrowserRouter>
 	);
 };
